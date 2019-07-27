eaeb76b77a09c538b4e5d7c4769b53f433408e7c
Daniel Arboleda <jarbol16@eafit.edu.co>
2019-07-27 00:41:47 -0500

    validation for borrowed book and validation for type of data

--- BibliotecaDominio/Bibliotecario.cs
+++ BibliotecaDominio/Bibliotecario.cs
@@ -7,6 +7,7 @@ namespace BibliotecaDominio
 {
     public class Bibliotecario
     {
+        public const string ISBN_NOT_FORMAT = "El valor ingresado no es un ISBN";
         public const string EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE = "El libro no se encuentra disponible";
         private  IRepositorioLibro libroRepositorio;
         private  IRepositorioPrestamo prestamoRepositorio;
@@ -17,6 +18,11 @@ namespace BibliotecaDominio
             this.prestamoRepositorio = prestamoRepositorio;
         }
 
+        /// <summary>
+        /// Metodo de pestamo de libro
+        /// </summary>
+        /// <param name="isbn">identifiacador de libro</param>
+        /// <param name="nombreUsuario">Nombre del usuario</param>
         public void Prestar(string isbn, string nombreUsuario)
         {
             throw new Exception("se debe implementar este método");
@@ -25,6 +31,14 @@ namespace BibliotecaDominio
 
         public bool EsPrestado(string isbn)
         {
+            if (isbn.IsNumber())
+            {
+                return this.prestamoRepositorio.ObtenerLibroPrestadoPorIsbn(isbn) == null ? false : true;
+            }
+            else
+            {
+                throw new Exception(ISBN_NOT_FORMAT);
+            }
             return false;
         }
     }
---
+++ BibliotecaDominio/Functions/Validaciones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaDominio
+{
+    /// <summary>
+    /// Clase que contione funciones de validaciones de datos para la biblioteca
+    /// </summary>
+    public static class Validaciones
+    {
+        /// <summary>
+        /// MEtodo extensivo para validar que un string sea solo numeros
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNumber(this string str)
+        {
+            return long.TryParse(str, out _);
+        }
+    }
+}
